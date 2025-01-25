2f5c2f3b484168137894565e2b38bee5f802847b
oktaycopurlu1 <oktaycopurlu1@gmail.com>
2025-01-25 10:27:04 +0100

    finally

--- index.js
+++ index.js
@@ -1,28 +1,12 @@
-const { RestClientV5 } = require("bybit-api");
 const express = require("express");
 require("dotenv").config();
 const { credentialsJson, listMessages, authorize } = require("./readEmail");
-const { placeOrder } = require("./placeOrder");
-const { BYBIT_API_KEY, BYBIT_API_SECRET, useTestnet } = require("./constants");
-
-const bybitClient = new RestClientV5({
-  key: BYBIT_API_KEY,
-  secret: BYBIT_API_SECRET,
-  testnet: useTestnet,
-});
 
 const app = express();
 app.use(express.json());
 
 app.post("/webhook", async (req, res) => {
   authorize(credentialsJson, listMessages);
-
-  try {
-    const parsedMessage = req.body;
-    console.log("Received message:", parsedMessage);
-  } catch (error) {
-    console.error("Error processing webhook:", error);
-  }
   res.status(204).send();
 });
 
